e515b8b2a339f365b4385228614d264071f30d35
hanwei <924856501@qq.com>
2018-12-19 19:29:06 +0800

    vue

---
+++ sail-kotlin/src/main/kotlin/com/study/spring/sailkotlin/SailKotlinApplication.java
@@ -0,0 +1,14 @@
+package com.study.spring.sailkotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailKotlinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SailKotlinApplication.class, args);
+    }
+
+}
+
